2f0bf4015a0f899af4bac256000c07e3821ab1a7
Evanthios Papadopoulos <papevanthios@gmail.com>
2022-03-28 10:48:19 +0300

    Initial commit

---
+++ src/Chapter1/IntQuest1.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+/**
+ * The IntQuest1 program implemented in Java for learning purposes.
+ *
+ * @author Evanthios Papadopoulos
+ * @since 28-Mar-22
+ **/
+
+public class IntQuest1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
