bd65474a637bb3c907aecc1249f27b20e90620e9
Evanthios Papadopoulos <papevanthios@gmail.com>
2022-03-31 10:19:20 +0300

    Added halfFinished TreeNode class.

---
+++ src/CtCILibrary/TreeNode.java
@@ -0,0 +1,28 @@
+package CtCILibrary;
+
+/**
+ * The TreeNode program implemented in Java for learning purposes.
+ *
+ * While it's perfectly fine - even good - to use the built-in binary tree class when possible, it's not always
+ * possible. In many questions, we needed access to the internals of the node or tree class (or needed to tweak these)
+ * and thus couldn't use the built-in libraries.
+ *
+ * The TreeNode class supports a variety of functionality, much of which we wouldn't necessarily want for every
+ * question/solution. For example, the TreeNode class tracks the parent of the node, even though we often don't use it
+ * (or specifically ban using it).
+ *
+ * The tree is implemented to be a binary search tree. However, you can use it for other purposes. You would just need
+ * to use the setLeftChild/setRightChild methods, or the left and right child variables. For this reason, we have kept
+ * these methods and variable public. We need this sort of access for many problems.
+ *
+ * @author Evanthios Papadopoulos
+ * @since 31-Mar-22
+ **/
+
+public class TreeNode {
+
+    public static void main(String[] args) {
+
+    }
+
+}
